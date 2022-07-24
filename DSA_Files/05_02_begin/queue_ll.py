7c22f8af544356c42b38f10ffff39466aca2b4fc
Ho1yShif <shifraisaacs@gmail.com>
2022-07-23 17:23:16 -0700

    Queues using deque and list

---
+++ DSA_Files/05_02_begin/queue_ll.py
@@ -0,0 +1,65 @@
+"""
+Python Data Structures - A Game-Based Approach
+Queue class
+Robin Andrews - https://compucademy.net/
+"""
+
+from collections import deque
+
+# Queue using the collections deque structure
+class Queue_d:
+	def __init__(self):
+		self.items = deque()
+	
+	def is_empty(self):
+		# empty deque returns False
+		return not self.items
+	
+	def enqueue(self, item):
+		self.items.append(item)
+	
+	def dequeue(self):
+		return self.items.popleft()
+	
+	def size(self):
+		return len(self.items)
+
+	def peek(self):
+		return self.items[0]
+	
+	def __str__(self) -> str:
+		return str(self.items)
+
+# Queue using the list structure
+class Queue_l:
+	def __init__(self):
+		self.items = []
+	
+	def is_empty(self):
+		# empty deque returns False
+		return not self.items
+	
+	def enqueue(self, item):
+		self.items.append(item)
+	
+	def dequeue(self):
+		return self.items.pop(0)
+	
+	def size(self):
+		return len(self.items)
+
+	def peek(self):
+		return self.items[0]
+	
+	def __str__(self) -> str:
+		return str(self.items)
+
+if __name__ == '__main__':
+	q = Queue_l()
+	
+	q.enqueue('I')
+	q.enqueue('Love')
+	q.enqueue('Zack')
+
+	print(q.dequeue(), q.dequeue(), q.dequeue())
+	print(q.is_empty())
