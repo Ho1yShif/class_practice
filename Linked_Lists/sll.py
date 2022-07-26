ad1c8f76fe695f5b2e71127e1377e11e8f516090
Ho1yShif <shifraisaacs@gmail.com>
2022-07-26 10:15:33 -0700

    singly linked list class

--- Linked_Lists/sll.py
+++ Linked_Lists/sll.py
@@ -1,29 +1,53 @@
-from dataclasses import dataclass
-
-# Singly Linked List Node Object
 class SLLNode:
 
 	def __init__(self, data):
 		self.data = data
 		self.next = None
-	
-	def __repr__(self) -> str:
-		return "SLL Node Object: data={}".format(self.data)
+
+	def __repr__(self):
+		return "SLLNode object: data={}".format(self.data)
 
 	def get_data(self):
+		"""Return the self.data attribute."""
 		return self.data
-	
+
 	def set_data(self, new_data):
+		"""Replace the existing value of the self.data attribute with new_data
+		parameter."""
 		self.data = new_data
-	
+
 	def get_next(self):
+		"""Return the self.next attribute"""
 		return self.next
-	
+
 	def set_next(self, new_next):
+		"""Replace the existing value of the self.next attribute with new_next
+		parameter."""
 		self.next = new_next
 
+class SLL:
+	def __init__(self):
+		self.head = None
+	
+	def __repr__(self) -> str:
+		return "SLLNode object: head={}".format(self.head)
+	
+	def is_empty(self):
+		return not self.head
+	
+	def add_front(self, new_data):
+		pass
+
+	def size(self):
+		pass
+
+	def search(self, data):
+		pass
+
+	def remove(self, data):
+		pass
+		
+
+
 if __name__ == '__main__':
-	node = SLLNode('Shif')
-	node2 = SLLNode('Zack')
-	node.set_next(node2)
-	node.get_next()
+	print(bool(None))
