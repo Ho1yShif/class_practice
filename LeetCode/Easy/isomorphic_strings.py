55a3054f4022413979cb5962e8f839b463c8d1f5
Ho1yShif <shifraisaacs@gmail.com>
2022-08-27 15:54:43 -0700

    isomorphic strings

---
+++ LeetCode/Easy/isomorphic_strings.py
@@ -0,0 +1,44 @@
+"""
+Problem #205: Isomorphic Strings
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+"""
+# Input
+s = "egg"
+t = "add"
+
+class Solution(object):
+	def isIsomorphic(self, s, t):
+		"""
+		:type s: str
+		:type t: str
+		:rtype: bool
+		"""
+		mapping_s_t = {}
+		mapping_t_s = {}
+		
+		for s_char, t_char in zip(s, t):
+			# If letters aren't mapped yet...
+			if (not s_char in mapping_s_t) and (not t_char in mapping_t_s):
+				mapping_s_t[s_char] = t_char
+				mapping_t_s[t_char] = s_char
+			
+			# If one letter doesn't map to the corresponding letter in the other string
+			elif mapping_s_t.get(s_char) != t_char or mapping_t_s.get(t_char) != s_char:
+				return False
+		
+		return True
+
+if __name__ == '__main__':
+	sol = Solution()
+	ans = sol.isIsomorphic(s, t)
+	print(ans)
+
+"""
+Solution Stats
+Runtime: 47 ms, faster than 61.21% of Python online submissions for Isomorphic Strings.
+Memory Usage: 16.2 MB, less than 23.30% of Python online submissions for Isomorphic Strings.
+"""
