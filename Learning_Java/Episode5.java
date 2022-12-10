7da3b14f2f76deaccf8e6a07b89a1015bd73d8c5
Ho1yShif <shifraisaacs@gmail.com>
2022-12-10 18:16:58 -0500

    cleanup

--- Learning_Java/Episode5.java
+++ Learning_Java/Episode5.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Episode5 {
 	public static void main(String[] args) {
-
 	System.out.println("Enter your first name ");
 	Scanner scan = new Scanner(System.in);
 	String scanFirstWord = scan.next();
