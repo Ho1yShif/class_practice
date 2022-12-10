a4626d1218330cf0d990d93cb1ba70a266a0a853
Ho1yShif <shifraisaacs@gmail.com>
2022-12-10 18:19:26 -0500

    cleanup

--- Learning_Java/Episode5.java
+++ Learning_Java/Episode5.java
@@ -4,8 +4,8 @@ public class Episode5 {
 	public static void main(String[] args) {
 	System.out.println("Enter your first name ");
 	Scanner scan = new Scanner(System.in);
-	String scanFirstWord = scan.next();
-	System.out.println(scanFirstWord);
+	String scanWord = scan.next();
+	System.out.println(scanWord);
 
 	System.out.println("Enter a full sentence");
 	String scanLine = scan.nextLine();
