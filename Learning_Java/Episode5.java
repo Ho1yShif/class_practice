73360e1fc423fe5aba770ff1319e8915f8fd5a5e
Ho1yShif <shifraisaacs@gmail.com>
2022-12-10 19:08:25 -0500

    fix

--- Learning_Java/Episode5.java
+++ Learning_Java/Episode5.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
 public class Episode5 {
+	// Name
 	public static void main(String[] args) {
 	System.out.println("Enter your first name ");
 	Scanner scan = new Scanner(System.in);
 	String scanWord = scan.next();
 	System.out.println(scanWord);
 
+	// Dummy nextline
+	scan.nextLine();
+	
+	// Sentence
 	System.out.println("Enter a full sentence");
 	String scanLine = scan.nextLine();
 	System.out.println(scanLine);
