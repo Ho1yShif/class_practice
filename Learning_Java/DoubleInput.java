675306bec4e3a16d68c1503f6a3c17754588ae13
Ho1yShif <shifraisaacs@gmail.com>
2022-12-14 10:21:19 -0500

    cleanup

--- Learning_Java/DoubleInput.java
+++ Learning_Java/DoubleInput.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
 public class DoubleInput {
-	// Name
 	public static void main(String[] args) {
-	// Int
+	// Double
 	System.out.println("What was your GPA in college on a scale of 0.0 to 4.0?");
 	Scanner scan = new Scanner(System.in);
 	double scanDouble = scan.nextDouble();
