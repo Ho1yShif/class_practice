a715ad2db04fec9a86b84e56fe950302195d12be
Ho1yShif <shifraisaacs@gmail.com>
2022-12-26 22:56:29 -0500

    finished room

--- Learning_Java/Episode5/Room.java
+++ Learning_Java/Episode5/Room.java
@@ -5,16 +5,15 @@ public class Room {
 	public static void main(String[] args) {
 	// Goal: Find the area of a room in square feet
 
-	// Fix this part
-	System.out.println("What is the length of your room?");
+	System.out.println("What is the length of your room in feet?");
 	Scanner scan = new Scanner(System.in);
 	int roomLength = scan.nextInt();
 
-	System.out.println("What is the width of your room?");
+	System.out.println("What is the width of your room in feet?");
 	double roomWidth = scan.nextInt();
 
 	double roomArea = roomLength * roomWidth;
-	System.out.println("The area of your room is " + roomArea + "in square feet");
+	System.out.println("The area of your room is " + roomArea + " square feet");
 	scan.close();
 	} 
 }
