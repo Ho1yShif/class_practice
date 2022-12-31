2e2cdbadf1725f7a3800ef3b80c2ddf62893b31c
Ho1yShif <shifraisaacs@gmail.com>
2022-12-31 18:44:33 -0500

    new nerdfighter example

--- Learning_Java/Episode8/not.java
+++ Learning_Java/Episode8/not.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class not {
 	public static void main(String[] args) {
+	System.out.println("How many hours have you spent watching Vlogbrothers videos?");
 	Scanner scan = new Scanner(System.in);
-	System.out.println("How many books by the Green brothers have you read?");
-	int greenBooksRead = scan.nextInt();
+	int vlogbrosWatchTime = scan.nextInt();
 
 	// NOT operator
-	if (!(greenBooksRead == 0)) {
-		System.out.println("You're a Nerdfighter!");
+	if (!(vlogbrosWatchTime == 100)) {
+		System.out.println("You're not a Nerdfighter yet!");
 	} else {
 		System.out.println("You're not a Nerdfighter yet!");
 	}
 
 	scan.close();
-	}
+	} 
 }
