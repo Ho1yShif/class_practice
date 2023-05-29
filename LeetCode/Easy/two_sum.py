f323e355f580123f16119b2d1f6382e433415340
Ho1yShif <shifraisaacs@gmail.com>
2023-05-28 21:41:39 -0400

    Add binary search solution to TwoSum problem

--- LeetCode/Easy/two_sum.py
+++ LeetCode/Easy/two_sum.py
@@ -9,24 +9,42 @@ Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 """
 
-nums = [3,2,4]
+nums = [2,7,11,15]
 
 class Solution(object):
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for outer_idx, outer_num in enumerate(nums):
-            start = outer_idx
-            remaining = target - nums[start]
-            
-            for nested_idx, nested_num in enumerate(nums[(outer_idx+1):]):
-                if nested_num == remaining:
-                    # idx of nested loop + start outer idx + 1 from enumerate
-                    end = nested_idx + outer_idx + 1
-                    return [start, end]
-
+	def TwoSumBruteForce(self, nums: list[int], target: int) -> list[int]:
+		for outer_idx, outer_num in enumerate(nums):
+			start = outer_idx
+			remaining = target - nums[start]
+			
+			for nested_idx, nested_num in enumerate(nums[(outer_idx+1):]):
+				if nested_num == remaining:
+					# idx of nested loop + start outer idx + 1 from enumerate
+					end = nested_idx + outer_idx + 1
+					return [start, end]
+	
+	def TwoSumBinarySearch(self, nums: list[int], target: int) -> list[int]:
+		nums = sorted(nums)
+		left = 0
+		right = len(nums) - 1
+		# Binary search
+		while left < right:
+			print(left, right)
+			current_sum = nums[left] + nums[right]	
+			if current_sum == target:
+				return [left, right]
+			elif current_sum < target:
+				left += 1
+			elif current_sum > target:
+				right -= 1
+			# Otherwise return empty list
+		else:
+			return []
 
 if __name__ == '__main__':
 	sol = Solution()
-	ans = sol.twoSum(nums, 6)
+	# ans = sol.TwoSumBruteForce(nums, 9)
+	ans = sol.TwoSumBinarySearch(nums, 9)
 	print(ans)
 
 """
