ed04e688b56f17e02bf739a8c94da143468da873
Ho1yShif <shifraisaacs@gmail.com>
2023-07-04 15:37:35 -0700

    Create authorized endpoint with template,view,url

--- django_smartnotes/home/views.py
+++ django_smartnotes/home/views.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
 	return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+# Block access to webpage if a user isn't logged in
+@login_required
+def authorized(request):
+	return render(request, 'home/authorized.html', {})
