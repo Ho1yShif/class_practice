2e30fbe5056593f8be894eab2d2f7e205dac19e3
Ho1yShif <shifraisaacs@gmail.com>
2023-07-09 13:23:53 -0700

    Convert indent to tabs for migrations

--- django_smartnotes/notes/migrations/0001_initial.py
+++ django_smartnotes/notes/migrations/0001_initial.py
@@ -5,19 +5,19 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    initial = True
+	initial = True
 
-    dependencies = [
-    ]
+	dependencies = [
+	]
 
-    operations = [
-        migrations.CreateModel(
-            name='Notes',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=200)),
-                ('text', models.TextField()),
-                ('created', models.DateTimeField(auto_now_add=True)),
-            ],
-        ),
-    ]
+	operations = [
+		migrations.CreateModel(
+			name='Notes',
+			fields=[
+				('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+				('title', models.CharField(max_length=200)),
+				('text', models.TextField()),
+				('created', models.DateTimeField(auto_now_add=True)),
+			],
+		),
+	]
