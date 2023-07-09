2380913f391562ef64582a4b995e716f845ede8f
Ho1yShif <shifraisaacs@gmail.com>
2023-07-09 13:22:00 -0700

    Add notes model

--- django_smartnotes/notes/models.py
+++ django_smartnotes/notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Notes(models.model):
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
