74001237a7ab2ef261622ed8f72e69287f6bda05
Shifra Isaacs <shifraisaacs@gmail.com>
2023-08-16 17:51:17 -0700

    Fix notes views.py

--- django_smartnotes/notes/views.py
+++ django_smartnotes/notes/views.py
@@ -6,5 +6,5 @@ def list(request):
 	return render(request, 'notes/notes_list.html', {'notes': all_notes})
 
 def detail(request, pk):
-	note = notes.objects.get(pk=pk)
+	note = Notes.objects.get(pk=pk)
 	return render(request, 'notes/notes_detail.html', {'note': note})
