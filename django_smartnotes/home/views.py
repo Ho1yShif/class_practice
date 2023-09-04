31c9bcd49d1d28a3681ce3c71d469e39cd0b7c6e
Shifra Isaacs <shifraisaacs@gmail.com>
2023-09-04 16:46:49 -0700

    Convert login auth view to class-based

--- django_smartnotes/home/views.py
+++ django_smartnotes/home/views.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 
-from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins	import LoginRequiredMixin
 
 class HomeView(TemplateView):
 	template_name = 'home/welcome.html'
 	extra_context = {'today': datetime.today()}
 
-# Block access to webpage if a user isn't logged in; redirect them to the login page
-@login_required(login_url='/admin')
-def authorized(request):
-	return render(request, 'home/authorized.html', {})
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+	template_name = 'home/authorized.html'
+	login_url = '/admin'
