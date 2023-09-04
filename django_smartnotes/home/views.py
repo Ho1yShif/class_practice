879b42a9c1f027ad7e94075ad3a90fe016cd03f2
Shifra Isaacs <shifraisaacs@gmail.com>
2023-09-04 16:41:09 -0700

    Convert views in home app to class-based view

--- django_smartnotes/home/views.py
+++ django_smartnotes/home/views.py
@@ -3,10 +3,11 @@ from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-
-def home(request):
-	return render(request, 'home/welcome.html', {'today': datetime.today()})
+class HomeView(TemplateView):
+	template_name = 'home/welcome.html'
+	extra_context = {'today': datetime.today()}
 
 # Block access to webpage if a user isn't logged in; redirect them to the login page
 @login_required(login_url='/admin')
