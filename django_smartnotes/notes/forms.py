812f95270d51afff723da79fd9b0e1b82b0981ea
Shifra Isaacs <shifraisaacs@gmail.com>
2023-09-08 16:48:59 -0700

    Format title and text fields with widgets

--- django_smartnotes/notes/forms.py
+++ django_smartnotes/notes/forms.py
@@ -6,6 +6,10 @@ class NotesForm(forms.ModelForm):
 	class Meta():
 		model = Notes
 		fields = ('title', 'text')
+		widgets = {
+			'title': forms.TextInput(attrs={'class': 'form-control my-5'}),
+			'text': forms.Textarea(attrs={'class': 'form-control my-5'})
+		}
 		labels = {
 			'text': 'Write your thoughts here:'
 		}
