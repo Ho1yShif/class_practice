a89a90d8d88b8165eed7a12df9704fa206d8c3f0
Shifra Isaacs <shifraisaacs@gmail.com>
2023-09-08 17:36:03 -0700

    Add LOGIN_REDIRECT_URL

--- django_smartnotes/smartnotes/settings.py
+++ django_smartnotes/smartnotes/settings.py
@@ -131,3 +131,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to notes page on successful login in order to avoid 404 errors
+LOGIN_REDIRECT_URL = '/smart/notes'
