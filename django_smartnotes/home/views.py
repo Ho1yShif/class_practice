b9012aa9ad83c4616ce2d32b67d66d58d40698b0
Shifra Isaacs <shifraisaacs@gmail.com>
2023-09-08 17:45:49 -0700

    Create SignupView

--- django_smartnotes/home/views.py
+++ django_smartnotes/home/views.py
@@ -1,10 +1,16 @@
 from datetime import datetime
-
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins	import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+
+class SignupView(CreateView):
+	form_class = UserCreationForm
+	template_name = 'home/register.html'
+	success_url = '/smart/notes'
 
 class LogoutInterfaceView(LogoutView):
 	template_name = 'home/logout.html'
