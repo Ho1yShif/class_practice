b07825afbf07b84b066a97f811046adb83d31fb0
Shifra Isaacs <shifraisaacs@gmail.com>
2023-09-08 17:31:27 -0700

    Create LoginInterfaceView

--- django_smartnotes/home/views.py
+++ django_smartnotes/home/views.py
@@ -4,7 +4,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins	import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 
+class LoginInterfaceView(LoginView):
+	template_name = 'home/login.html'
+	
 class HomeView(TemplateView):
 	template_name = 'home/welcome.html'
 	extra_context = {'today': datetime.today()}
